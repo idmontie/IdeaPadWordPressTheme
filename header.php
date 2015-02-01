d45a49b5ddbf368c0c2ddd0faf75f852f9ce309c
Ivan Montiel <idmontie@asu.edu>
2015-01-31 22:06:31 -0700

    typography changes

--- header.php
+++ header.php
@@ -13,24 +13,41 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'idea-pad' ); ?></a>
+  <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'idea-pad' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
+  <?php if ( is_front_page() || is_home() ) : ?>
+    <?php
+      $image = '';
+      $image = get_post_meta( get_the_ID(), 'hero-image' );
+      if ( isset( $image ) && ! empty( $image ) ) {
+        $image = $image[0];
+      }
+    ?>
+    <?php if ( isset( $image ) && ! empty( $image ) ) : ?>
+      <header id="masthead" class="site-header" role="banner" style="background-image: url( <?php echo image; ?>)">
+    <?php else: ?>
+      <header id="masthead" class="site-header" role="banner">
+    <?php endif; ?>
+      <div class="site-branding">
+        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+      </div><!-- .site-branding -->
+    </header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'idea-pad' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    <nav id="site-navigation" class="main-navigation" role="navigation">
+      <div class="content">
+        <h3 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h3>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 
-	<div id="content" class="site-content">
+        <?php get_sidebar(); ?>
+      </div>
+    </nav><!-- #site-navigation -->
+  <?php endif; ?>
+
+  <div id="content" class="site-content">
