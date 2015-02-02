e53af72cb025bcb81d9388a5cd66e4f40b020743
Ivan Montiel <idmontie@asu.edu>
2015-02-01 20:37:42 -0700

    minor fix to touch events

--- js/menu.js
+++ js/menu.js
@@ -6,57 +6,59 @@
 ( function() {
   'use strict';
 
-  var fixedBackground = function () {
-    $( 'body' ).css( {
-      'max-height' : '100%',
-      'overflow' : 'hidden'
-    } );
-  };
+  jQuery( function ( $ ) {
+    var fixedBackground = function () {
+      $( 'body' ).css( {
+        'max-height' : '100%',
+        'overflow' : 'hidden'
+      } );
+    };
 
-  var scrollBackground = function () {
-    $( 'body' ).css( {
-      'max-height' : 'auto',
-      'overflow' : 'auto'
-    } );
-  }
-
-  var navSelector = 'nav#site-navigation';
-
-  $( navSelector ).click( function () {
-    // Open up the navigation
-    $( this ).addClass( 'hover' );
-
-    $( this ).find( '.content' ).css( {
-      display: 'block'
-    } );
-
-    fixedBackground();
-  } ).hover( function () {
-    // ========
-    // Hover In
-    // ========
-    fixedBackground();
-  }, function () {
-    // =========
-    // Hover Out
-    // =========
-    scrollBackground();
-  } )
-  $( document ).on( {
-    'touchend, mouseup' : function ( e ) {
-      var container = $( navSelector );
-
-      if ( ! container.is( e.target )
-          && container.has( e.target ).length === 0) {
-        scrollBackground();
-
-        // Close the navigation
-        container.removeClass( 'hover' );
-
-        container.find( '.content' ).css( {
-          display: 'none'
-        } );
-      }
+    var scrollBackground = function () {
+      $( 'body' ).css( {
+        'max-height' : 'auto',
+        'overflow' : 'auto'
+      } );
     }
+
+    var navSelector = 'nav#site-navigation';
+
+    $( navSelector ).click( function () {
+      // Open up the navigation
+      $( this ).addClass( 'hover' );
+
+      $( this ).find( '.content' ).css( {
+        display: 'block'
+      } );
+
+      fixedBackground();
+    } ).hover( function () {
+      // ========
+      // Hover In
+      // ========
+      fixedBackground();
+    }, function () {
+      // =========
+      // Hover Out
+      // =========
+      scrollBackground();
+    } )
+    $( document ).on( {
+      'touchend mouseup' : function ( e ) {
+        var container = $( navSelector );
+
+        if ( ! container.is( e.target )
+            && container.has( e.target ).length === 0) {
+          scrollBackground();
+
+          // Close the navigation
+          container.removeClass( 'hover' );
+
+          container.find( '.content' ).css( {
+            display: 'none'
+          } );
+        }
+      }
+    } );
   } );
 } )();
